04207609e8524b04217728bef8e07a1feffaf42f
K1000KN <k1000kn@protonmail.com>
2022-01-26 15:26:37 +0100

    step01 done

---
+++ Character.rb
@@ -0,0 +1,43 @@
+class Character
+  def initialize(name, rpg_class)
+    @name = name
+    @life = 50
+    @agility = 2
+    @strength = 2
+    @wit = 2
+    @rpg_class = rpg_class
+  end
+
+  def name
+    @name
+  end
+
+  def life
+    @life
+  end
+
+  def agility
+    @agility
+  end
+
+  def strength
+    @strength
+  end
+
+  def wit
+    @wit
+  end
+
+  def rpg_class
+    @rpg_class
+  end
+
+  def attack(string)
+    print "#{@name}: Rrrrrrrrr ...."
+  end
+end
+
+ perso = Character.new("Marc", "Guardian")
+
+ puts(perso.name, perso.life, perso.agility, perso.strength, perso.wit, perso.rpg_class)
+ perso.attack("maincelesteX!!!")
